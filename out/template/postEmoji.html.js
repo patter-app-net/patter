36479bc0b86f0a72ba8b188ec545c4ece9795f5d
Jonathon Duerig <duerig@flux.utah.edu>
2013-03-06 18:50:30 -0700

    Remove output files.

--- out/template/postEmoji.html.js
+++
@@ -1,1 +0,0 @@
-define('text!template/postEmoji.html',[],function () { return '<img class="emoji" width="24" height="24">\r\n';});
--- out/template/user.html.js
+++
@@ -1,1 +0,0 @@
-define('text!template/user.html',[],function () { return '<div>\r\n  <a class="userAvatar" target="_blank">\r\n    <img class="userAvatarImg" width="33" height="33">\r\n  </a>\r\n  <a class="userName" href=""></a>\r\n</div>\r\n';});
