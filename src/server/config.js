52fbbfe2c5b312d5cb26ee82581f78eb16b9068e
Jonathon Duerig <duerig@flux.utah.edu>
2013-09-07 16:12:46 -0600

    Remove config file from repo. Remove obsolete formatting of logo.

--- src/server/config.js
+++
@@ -1,5 +0,0 @@
-exports.port = 80;
-exports.ip = '172.31.1.189';
-exports.token = 'AQAAAAAAB6h2MShZj3r0IKONldxrllg5_kwrEphb06RHmVBKjMOy2EvNJUgr6d5PuKlx_1k5RQ8o2VNFrFdmBoZl36YD4lR0wA';
-
-exports.appnetPath = '../dist/js/deps/appnet';
